544497788046157859d5a7e0f766b84b81c96b3a
Merlin <merlin.hellbach@icloud.com>
2024-11-05 11:53:56 +0100

    uhr eingesetzt

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/time", (req, res) => {
+  const now = new Date();
+  const time = now.toLocaleTimeString(); // Gibt die aktuelle Zeit im lokalen Format zurück
+  res.send(`Current server time is: ${time}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Express App is running on http://localhost:${PORT}`);
 });
+
